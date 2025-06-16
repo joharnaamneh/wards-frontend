4c18232c52c28b93e71f5eb8d04e750d23720979
Johar Naamneh <johar.naamneh@bit.admin.ch>
2025-06-16 14:22:29 +0200

    Added Outfits

---
+++ app/(tabs)/outfit.tsx
@@ -0,0 +1,9 @@
+import {ThemedView} from "@/components/ThemedView";
+import {ThemedText} from "@/components/ThemedText";
+export default function OutfitScreen() {
+    return (
+        <ThemedView>
+            <ThemedText type="title">Outfits</ThemedText>
+        </ThemedView>
+    );
+}
